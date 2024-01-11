215f96bc514d2876505434cd514ed2dfd03d187f
rnarang <rnarang@LAP-55379>
2024-01-11 10:54:59 +0530

    Added files

--- advanced_python_development/times/time_folder.py
+++ advanced_python_development/times/time_folder.py
@@ -3,7 +3,7 @@ def measure_runtime(func):
     start = time.time()
     func()
     end = time.time()
-    print(end - start)
+    #print(end - start)
 
 def powers(limit):
     return [x ** 2 for x in range(limit)]
