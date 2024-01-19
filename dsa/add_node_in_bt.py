17c56fe53f842014fe74da1357b84b9b8653cc46
rnarang <rnarang@LAP-55379>
2024-01-19 17:12:47 +0530

    Added files

---
+++ dsa/add_node_in_bt.py
@@ -0,0 +1,2 @@
+new_node=Node(value=5,left=None,right=None)
+current_node=self.head
---
+++ libraries/app.py
@@ -0,0 +1,10 @@
+"""
+This is wonderful python.
+
+"""
+from flask import Flask
+
+APP=Flask(__name__)
+
+if __name__=='__main__':
+    APP.run()
